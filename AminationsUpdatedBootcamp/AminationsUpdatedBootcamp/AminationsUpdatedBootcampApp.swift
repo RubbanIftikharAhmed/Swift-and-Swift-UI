1657231136a5281b11cfe945d6436da07abe1e28
RubbanIftikhar <iamrubban@gmail.com>
2025-06-12 14:42:25 +0500

    Intermediate SwiftUI concepts

---
+++ AminationsUpdatedBootcamp/AminationsUpdatedBootcamp/AminationsUpdatedBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  AminationsUpdatedBootcampApp.swift
+//  AminationsUpdatedBootcamp
+//
+//  Created by Rubban Iftikhar on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AminationsUpdatedBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AminationsUpdatedBootcamp/AminationsUpdatedBootcampTests/AminationsUpdatedBootcampTests.swift
@@ -0,0 +1,17 @@
+//
+//  AminationsUpdatedBootcampTests.swift
+//  AminationsUpdatedBootcampTests
+//
+//  Created by Rubban Iftikhar on 31/07/2024.
+//
+
+import Testing
+@testable import AminationsUpdatedBootcamp
+
+struct AminationsUpdatedBootcampTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
